16a2fb55ca58f95a3d8de88acab678c9b79e2d31
Patrick Ashamalla <patrick.ashamalla@gmail.com>
2017-03-06 11:32:17 -0500

    Update to hard coded json

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 1,"firstName":"Robert","lastName":"Smith","email":"bob@gmail.com"},
     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
